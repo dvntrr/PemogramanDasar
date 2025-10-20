31dd66ff52b9a3e1a52b424865e2e58eb3518756
davauntaraa <davauntaraa@gmail.com>
2025-10-20 14:23:02 +0700

    Menambahkan Pertemuan 4

---
+++ Pertemuan4/programHitungGaji/dingindamai.py
@@ -0,0 +1,39 @@
+print("PROGRAM HITUNG GAJI KARYAWAN")
+nama = input("Masukkan Nama Karyawan : ")
+golongan = input("Masukkan Golongan Jabatan : ")
+pendidikan = input("Masukkan Pendidikan : ").upper()
+jk = int(input("Masukkan Jumlah Jam Kerja : "))
+gaji = 300000
+
+if golongan == "1" :
+    tj = 0.05*gaji
+elif golongan == "2" :
+    tj = 0.10*gaji
+elif golongan == "3" :
+    tj = 0.15*gaji
+else :
+    tj = 0
+
+if pendidikan == "SMA" :
+    tp = 0.025*gaji
+elif pendidikan == "D1" :
+    tp = 0.05*gaji
+elif pendidikan == "D3" :
+    tp = 0.2*gaji
+elif pendidikan == "S1" :
+    tp = 0.3*gaji
+else :
+    tp = 0
+
+jkl = jk-8
+lembur = jkl*3500
+total = lembur+tp+tj+gaji
+
+print("==========================================")
+print(f"Karyawan Yang Bernama {nama}")
+print(f"Honor Yang Diterima")
+print(f"    Tunjangan Jabatan    Rp{int(tj)}")
+print(f"    Tunjangan Pendidikan Rp{int(tp)}")
+print(f"    Honor Lembur         Rp{int(lembur)}")
+print(f"                         ________________+")
+print(f"Total Gaji               Rp{int(total)}")
